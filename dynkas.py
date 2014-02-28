a634c25fda1273be5639431b0cf30b56fe8ce494
Fabio Calefato <calefato.uniba@gmail.com>
2014-02-28 14:06:04 +0100

    Fixed issue #1 - Add command line switches with getopt

--- dynkas.py
+++ dynkas.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-"""DyndnsKeepAlive.
+"""
+	DyndnsKeepAlive script (dynkas)
 
-	Requires Python 3.x
-	IMAP and Dyndns accounts
+	Requires:
+	- Python 3.x
+	- a Dyndns account
+	- a Gmail account with IMAP  enabled
 	
 """
+import getopt
+import sys
 
 import smtplib
 import imaplib
@@ -28,12 +33,18 @@ SMTP_PORT = '587'
 
 class DyndnsKeepAlive(object):
 	
-	def __init__(self, user, password):
+	def __init__(self, user, password, debug, timedelta):
 		self.log = logging.getLogger('Dyndns auto keep-alive script')
-		self.log.setLevel(logging.DEBUG)
+		if(debug == True):
+			self.log.setLevel(logging.DEBUG)
+		else:
+			self.log.setLevel(logging.WARNING)
 		# add a file handler
 		fh = logging.handlers.RotatingFileHandler('dynkas.log')
-		fh.setLevel(logging.DEBUG)
+		if(debug == True):
+			fh.setLevel(logging.DEBUG)
+		else:
+			fh.setLevel(logging.WARNING)
 		# create a formatter and set the formatter for the handler.
 		fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 		# add the Handler to the logger
@@ -41,6 +52,7 @@ class DyndnsKeepAlive(object):
 
 		self.user = user
 		self.password = password
+		self.timedelta = timedelta
 				
 		self.smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
 		
@@ -111,6 +123,7 @@ class DyndnsKeepAlive(object):
 		# make sure it is marked as read
 		print (msg_uid)
 		# archive
+		# TODO internet/dyndns label should be an option from CL
 		typ, data = self.imap.uid('STORE', msg_uid, '+X-GM-LABELS', 'internet/dyndns')
 		# move from inbox
 		typ, data = self.imap.uid('STORE', msg_uid, '-X-GM-LABELS', '\Inbox')
@@ -137,8 +150,8 @@ class DyndnsKeepAlive(object):
 		# check for new mail from dyndns since last check (yesteday)
 		# TODO need to store last datetime instead of timedelta[1] (1 day) 
 		# as the email from dyndns is automatically sent 5 and 3 days before expiration
-		# setting a time delta back to 5 or 3 +1 days seems reasonable.
-		since_date = (date.today() - datetime.timedelta(5)).strftime("%d-%b-%Y")
+		# setting a time delta back to 5 - 3 days seems reasonable.
+		since_date = (date.today() - datetime.timedelta(self.timedelta)).strftime("%d-%b-%Y")
 		self.log.info("Searching for email in inbox since: %s\n" % (since_date))
 		
 		sent_from = 'donotreply@dyn.com'
@@ -169,31 +182,71 @@ class DyndnsKeepAlive(object):
 				html = urlopen(link).read().decode(encoding='UTF-8')
 				# parse the html to make sure that the account will be kept alive for the next 30 days
 				# self.log.debug(html)	
+				
 				# error msgs about multiple KA attempts on same link
 				error_msg1 = 'Error proccessing your host confirmation'
 				error_msg2 = 'Your host confirmation has already been completed'		
 				# confirmation of successful KA attempt
 				keepalive_msg1 = 'Account Activity Confirmed'
-				keepalive_msg2 = 'has been confirmed as active'				
+				keepalive_msg2 = 'has been confirmed as active'
+
 				if (error_msg1 in html and error_msg2 in html):
-					self.log.warning('The script has processed an old email from Dyndns. This shouldn\'t happen. To fix this, please, manually archive all Dyndns old emails from your Inbox.')
+					text = 'The script has processed an old email from Dyndns. This shouldn\'t happen.\nTo fix this, please, manually archive all Dyndns old emails from your Inbox.'
+					self.log.warning(text)
 				elif (keepalive_msg1 in html and keepalive_msg2 in html):
 					self.log.info('Everything went fine, sending confirmation by email and archiving the email')
 					# archive the dyndns request and send a notification by email that the script worked fine
-					self.archive_message(msg_id)
-					subject = 'Dyndns auto-keep-alive script notifcation'
+					# self.archive_message(msg_id)					
 					text = "The script was executed successfully on {date}.\n For more info, check this resource: http://github.com/dyndns-kas".format(date=datetime.datetime.now().strftime("%c"))
-					self.send_email(imap_username, imap_username, subject, text);
 				else:
-					self.log.error('Unknown error message please report the following error as an issue here at http://github.com/dyndns-kas\nError to report:\n\n{e}'.format(e=html))
+					# TODO send html code as a page attached
+					text = 'Unknown error message please report the following error as an issue here at http://github.com/dyndns-kas\nError to report:\n\n{e}'.format(e=html)
+					self.log.error(text)
+					
+				subject = 'Dyndns auto-keep-alive script notifcation'
+				self.send_email(self.user, self.user, subject, text)
 		else:
 			self.log.info('No emails from dyn.com found as of {d}'.format(d=date.today()))
 			
 		self.log.info('Exiting script at {now}'.format(now=datetime.datetime.now().strftime("%c")))
 	
 if __name__ == '__main__':
-	# example:
-	imap_username = 'dummy@gmail.com'
-	imap_password = '#secret'
-	with DyndnsKeepAlive(imap_username, imap_password) as kas:
+	
+	# default CL arg values
+	username = 'name@gmail.com'
+	password = 'secret'
+	debug = False
+	timedelta = 5
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hu:p:dt:",["help","username=","password=","debug","timedelta="])
+	except getopt.GetoptError:
+		print('Wrong arg. Please, enter dynkas.py [-h|--help] for usage info.')
+		sys.exit(2)
+		
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print('Usage: dynkas.py [OPTIONS]\n\n\
+					\t-h, --help                             prints this help\n\
+					\t-u, --username  <email@gmail>          your gmail address\n\
+					\t-p, --password  <secret>               your secret password\n\
+					\t-d, --debug                            shows debug info into log file\n\
+					\t-t, --timedelta <value between 3-5>    checks emails back to timedelta days')					
+			sys.exit()
+		elif opt in ("-u", "--username"):
+			username = arg
+		elif opt in ("-p", "--password"):
+			password = arg
+		elif opt in ("-d", "--debug"):
+			debug = True
+		elif opt in ("-t", "--timedelta"):
+			try:
+				timedelta = int(arg)
+			except exceptions.ValueError:
+				timedelta = 5
+				w = "Error parsing {td} into integer. Using default timedelta of {dtd}".format(td=arg, dtd=timedelta)
+				print(w)
+				self.log.warning(w)
+
+	with DyndnsKeepAlive(username, password, debug, timedelta) as kas:
 		kas.main()
