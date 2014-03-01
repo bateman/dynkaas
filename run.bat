64289f36c73291cd6ab8b754a56fbd101a3264b4
Fabio Calefato <calefato.uniba@gmail.com>
2014-03-01 22:15:51 +0100

    Added run scripts. Close issue #6

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+REM  Note: Replace values between <> before running
+REM        or run with -h, --help for more options
+python dynkas.py -uYourEnmail@gmail.com -pSecret
+@echo on
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Note: Replace values between <> before running
+#       or run with -h, --help for more options
+python dynkas.py -u<YourEmail>@gmail.com -p<Secret>
