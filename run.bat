6df1fc5ae66886e7362f47aec96f4b63e5322a3f
Fabio Calefato <fabio.calefato@uniba.it>
2014-03-05 13:41:07 +0100

    Wrong closing >

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 REM  Note: Replace values between <> before running
 REM        or run with -h, --help for more options
-python dynkaas.py -u<YourEnmail@gmail.com> -p<Secret>
+python dynkaas.py -u<YourEnmail>@gmail.com -p<Secret>
 @echo on
