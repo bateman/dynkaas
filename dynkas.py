928b75d394e7fcc1fca3b41382a2dff83b94c37a
Fabio Calefato <fabio.calefato@uniba.it>
2014-03-07 11:38:25 +0100

    Update lincence and author info

--- dynkas.py
+++ dynkas.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Fabio Calefato
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
 	Dyndns Keep Account Alive script (dynkaas) -	https://github.com/bateman/dynkaas
@@ -9,6 +29,9 @@
 	- a Gmail account with IMAP enabled
 	- Emails must be in Inbox (i.e., not archived, may also have other labels).	
 """
+
+__author__ = '@bateman'
+
 import getopt
 import sys
 import os
